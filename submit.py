f54d36375f7e93ea641686faf87bb27fda69c8bd
John Bridgers <john@macbook-pro-34.lan>
2024-09-29 11:50:10 -0400

    multiple alpha and beta values

--- submit.py
+++ submit.py
@@ -28,6 +28,9 @@ def main():
     two_and_five = ["C2", "C5"]
     not_two_and_five = list(set(all_cells) - set(two_and_five))
 
+    alphas = [0.001, 0.0001]
+    betas = [0.075]
+
 
     for i in [0,1,2]:
         # 0 - no correction, 1 - C19 corrected, 2 - both corrections
@@ -42,14 +45,6 @@ def main():
         for c in clades:
             path_raw = folder + files_raw[i]
             path_corrected = folder + files_corrected[i]
-            alpha = None
-            beta = None
-            if i == 0 or i == 1:
-                alpha = 0.001
-                beta = 0.075
-            else:
-                alpha = 0.0001
-                beta = 0.075
 
 
             df_corrected = pd.read_csv(path_corrected, sep="\t", index_col=[0]).sort_values(by=["cell_id_x_mut_id"])
@@ -66,19 +61,21 @@ def main():
                     muts += [col]
 
             for mut in muts:
-                cmd = "sbatch"
-                cmd += ' --job-name="' + 'job_partf.' + str(i) + '.' + cells + '.' + mut + '"'
-                cmd += ' --output="' + 'job_partf.' + str(i) + '.' + cells + '.' + mut + '.%j.out"'
-                cmd += ' --error="' + 'job_partf.' + str(i) + '.' + cells + '.' + mut + '.%j.err"'
-                cmd += ' --export=CELLS="' + cells + '"'
-                cmd += ',MUT="' + mut + '"'
-                cmd += ',ALPHA="' + str(alpha) + '"'
-                cmd += ',BETA="' + str(beta) + '"'
-                cmd += ',INPUT="' + path_raw + '"'
-                cmd += ',SAMPLES="' + str(num_samples) + '"'
-                cmd += ' run_partf.sbatch'
-                os.system(cmd)
-                print(cmd)
+                for alpha in alphas:
+                    for beta in betas:
+                        cmd = "sbatch"
+                        cmd += ' --job-name="' + 'job_partf.' + str(i) + '.' + cells + '.' + mut + '.' + str(alpha) + '.' + str(beta) + '"'
+                        cmd += ' --output="' + 'job_partf.' + str(i) + '.' + cells + '.' + mut + '.' + str(alpha) + '.' + str(beta) + '.%j.out"'
+                        cmd += ' --error="' + 'job_partf.' + str(i) + '.' + cells + '.' + mut + '.' + str(alpha) + '.' + str(beta) + '.%j.err"'
+                        cmd += ' --export=CELLS="' + cells + '"'
+                        cmd += ',MUT="' + mut + '"'
+                        cmd += ',ALPHA="' + str(alpha) + '"'
+                        cmd += ',BETA="' + str(beta) + '"'
+                        cmd += ',INPUT="' + path_raw + '"'
+                        cmd += ',SAMPLES="' + str(num_samples) + '"'
+                        cmd += ' run_partf.sbatch'
+                        os.system(cmd)
+                        print(cmd)
 
 if __name__ == "__main__":
     main()
